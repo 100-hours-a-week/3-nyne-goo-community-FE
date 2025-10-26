f532708fdb15ea7e1dc23efc163beb72156d927f
tn-als <kusumin38@gmail.com>
2025-10-26 17:38:52 +0900

    refactor: replace toast location

--- public/common/js/api.js
+++ public/common/js/api.js
@@ -13,21 +13,23 @@ export async function apiRequest(endpoint, options = {}) {
     });
 
     // 공통 에러 처리
-    // if (response.status === 401 || response.status === 403) {
-    //   // 로그인 페이지에서는 redirect 하지 않음
-    //   if (!(window.location.pathname.includes("/login")||window.location.pathname.includes("/signup"))) {
-    //     window.location.replace("/login");
-    //   } else {
-    //     throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
-    //   }
-    //   return null;
-    // }
+    if (response.status === 401 || response.status === 403) {
+      // 로그인 페이지에서는 redirect 하지 않음
+      if (!(window.location.pathname.includes("/login")||window.location.pathname.includes("/signup"))) {
+        window.location.replace("/login");
+      } else {
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+      }
+      return null;
+    }
 
     // JSON 파싱
     const data = await response.json();
 
     if (!(response.status === 200 || response.status === 201)) {
-      throw new Error(data.message || "API 요청 실패");
+      const error = new Error("API 요청 실패");
+      error.status = response.status;
+      throw error;
     }
 
     return data;
